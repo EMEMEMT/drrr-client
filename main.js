1f2bb45c30361b38e4ae133dd3b7c6c9f13e36f3
Megumi_mumu <megumimumu@outlook.com>
2026-05-29 20:56:57 +0800

    完成环境配置

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 350,            // 窗口宽度，类似手机屏幕
+    width: 350,            // 窗口宽度
     height: 600,           // 窗口高度
-    alwaysOnTop: true,     // 【关键】开启永远置顶
-    autoHideMenuBar: true, // 隐藏顶部的原生菜单栏，让界面更清爽
+    alwaysOnTop: true,
+    autoHideMenuBar: true, 
     webPreferences: {
-      nodeIntegration: false // 加载外部网页时的标准安全设置
+      nodeIntegration: false 
     }
   })
 
-  // 让这个窗口直接加载 drrr.com
+  // 加载 drrr.com
   win.loadURL('https://drrr.com')
 }
 
-// 当 Electron 初始化完成时，打开窗口
+// Electron 初始化完成时，打开窗口
 app.whenReady().then(() => {
   createWindow()
 
